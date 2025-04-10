444cc29c8b5876c07845f88cd0d33644f9784584
leticia510 <a.nunes@aluno.ifce.edu.br>
2025-04-10 12:25:07 -0300

    feat: Inicialização do projeto Django com configurações base

--- leticia.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git config --global user.name "leticia510"
-git config --global user.email "a.nunes@aluno.ifce.edu.br"
-cp ~/.ssh/20231321000027 ~/.ssh/id_ed25519
-cp ~/.ssh/20231321000027.pub ~/.ssh/id_ed25519.pub
-chmod 600 ~/.ssh/id_ed25519
-chmod 644 ~/.ssh/id_ed25519.pub
-echo "Config concluida"
